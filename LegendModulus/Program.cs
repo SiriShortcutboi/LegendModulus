e93991407f9eb708c107951d86f25e0d7445ce6d
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2026-02-15 00:24:11 -0700

    remember kids, int variables cannot contain string 0s like 0 they really do have to be just 0; bruh debugging is frustrating

---
+++ LegendModulus/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+					
+public class Program
+{
+	static string userInput = "default";
+    static int numberprocess = 0;
+   
+		//might have to declare an int for conversion and do try parse below
+	public static void Main()
+	{
+        Console.WriteLine("\nI am MODULUS, cruncher of numbers, enter your number to see its fate!");
+		userInput = Console.ReadLine();
+
+
+        int.TryParse(userInput, out numberprocess);
+
+        if (numberprocess % 2 == 0) {
+            Console.WriteLine($"{userInput} is Even");
+        }
+        else
+        {
+            Console.WriteLine($"{userInput} is Odd");
+        }
+		
+		
+	}
+}
