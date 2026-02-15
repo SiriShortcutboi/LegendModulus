f4f82fa2ae9c0e7c5e7b9d09bec6d1b0b8c8dbda
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2026-02-15 00:28:37 -0700

    added learning comments

--- LegendModulus/Program.cs
+++ LegendModulus/Program.cs
@@ -12,8 +12,9 @@ public class Program
         Console.WriteLine("\nI am MODULUS, cruncher of numbers, enter your number to see its fate!");
 		userInput = Console.ReadLine();
 
-
+    //int.TryParse is not something you rename every time you use it
         int.TryParse(userInput, out numberprocess);
+    //you just put a STRING variable in position 1 and OUT it to an int in position 2
 
         if (numberprocess % 2 == 0) {
             Console.WriteLine($"{userInput} is Even");
